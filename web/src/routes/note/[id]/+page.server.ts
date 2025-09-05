43a05e5f565a760afe7263ee4049da20352c1890
Luis Dourado <lfaddourado@gmail.com>
2025-09-05 17:02:04 -0300

    fix: style horizontal scroll on tables in mobile views

--- web/src/routes/note/[id]/+page.server.ts
+++ web/src/routes/note/[id]/+page.server.ts
@@ -11,7 +11,15 @@ export async function load({ params }) {
 			throw error(404, 'Note not found');
 		}
 
-		const content = marked.parse(note.content);
+		const renderer = new marked.Renderer();
+
+		const originalTable = renderer.table.bind(renderer);
+		renderer.table = function (table) {
+			const html = originalTable(table);
+			return `<div class="table-wrapper">${html}</div>`;
+		};
+
+		const content = marked.parse(note.content, { renderer });
 
 		return {
 			note,
